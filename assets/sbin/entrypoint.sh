cda50ed032c7a0d9ec4f4a8bd5a3609a6b9f8ebc
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2016-08-29 10:14:03 +0100

    Simplify entrypoint

--- assets/sbin/entrypoint.sh
+++ assets/sbin/entrypoint.sh
@@ -5,13 +5,9 @@ mkdir -p /run/redis
 chmod -R 755 /run/redis
 chown -R $REDIS_USER:$REDIS_USER /run/redis
 
-if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
-    set -- redis-server "$@"
-fi
-
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
     chown -R $REDIS_USER .
     exec gosu $REDIS_USER "$0" "$@"
+else
+    exec "$@"
 fi
-
-exec "$@"
